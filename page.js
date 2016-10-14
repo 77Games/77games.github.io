d9eea19fe4e937de290a68bad8a8d036937473a9
Eric O <edowaffles@gmail.com>
2016-10-14 14:58:42 -0500

    Update page.js

--- page.js
+++ page.js
@@ -1,1 +1,1 @@
-document.body.innerHTML='<ul id="nav"><li><a href="//77games.github.io">Home</a></li></ul><div class="c">'+document.body.innerHTML+ '</div><div class="footer"><p>Copyright 2016 77Games</p></div><link rel="stylesheet" type="text/css" href="//77games.github.io/theme.css">'
+document.body.innerHTML='<ul id="nav"><li><a href="//77games.github.io">Home</a></li></ul>'+document.body.innerHTML+ '<div class="footer"><p>Copyright 2016 77Games</p></div><link rel="stylesheet" type="text/css" href="//77games.github.io/theme.css">'
